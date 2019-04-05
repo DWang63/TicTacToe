8e89221f18ee57df062b2c36e39821bdde03b900
DWang63 <dw9331@gmail.com>
2019-04-04 21:55:45 -0400

    Started making a function for making buttons

--- game.py
+++ game.py
@@ -1,14 +1,21 @@
 import sys, pygame
-
 run = True
 pygame.init()
 size = width, height = 1000, 1000
+
 
 black = 0, 0, 0
 white = 255, 255, 255
 screen = pygame.display.set_mode(size)
 
+mouseposition = 0
 loadingscreen = True
+
+def button(x, y):
+    mouseposition = pygame.mouse.get_pos()
+    if event.type == pygame.mouse.get_pressed():
+        pygame.mouse.get_pos()
+        
 
 
 def drawboard():
@@ -16,7 +23,7 @@ def drawboard():
     board = pygame.transform.scale(board, (size))
     boardrect = board.get_rect()
     screen.blit(board, boardrect)
-    
+
 
 def drawmove():
     xicon = pygame.image.load("x.png")
@@ -24,13 +31,13 @@ def drawmove():
     xrect = xicon.get_rect()
     screen.blit(xicon, xrect)
 while run:
+    drawboard()
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
 
-        if event.type == pygame.mouse.get_pressed():
-            mouseposition = pygame.mouse.get_pos()
-            print (mouseposition)  
-    drawboard()
+        
+              
+    
     drawmove()
     pygame.display.update()
